ee907ae49d9c6a33abdd6598af31892c69d48089
Théophile Candelier <thophilecandelier@yahoo.fr>
2019-03-28 20:50:47 +0100

    Enemy now walks and detects player

---
+++ Source/Ombres/MyAIController.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyAIController.h"
+
+AMyAIController::AMyAIController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
+	SetGenericTeamId(FGenericTeamId(1));
+}
