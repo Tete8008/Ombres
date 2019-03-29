218d14e91fb30810dc758b9d2692fb377d2a9f7e
Lug <l.ambroise@rubika-edu.com>
2019-03-29 12:37:17 +0100

    Modfi C++ add header

--- Source/Ombres/MyAIController.cpp
+++ Source/Ombres/MyAIController.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MyAIController.h"
+#include "AIController.h"
 
 AMyAIController::AMyAIController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
 	SetGenericTeamId(FGenericTeamId(1));
