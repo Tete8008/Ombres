eca8958f077a18e99e1210648ec06f09587ec2e7
Théophile Candelier <thophilecandelier@yahoo.fr>
2019-03-29 19:07:41 +0100

    deleted c++ classes

--- Source/Ombres/MyAIController.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "MyAIController.h"
-#include "AIController.h"
-
-AMyAIController::AMyAIController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
-	SetGenericTeamId(FGenericTeamId(1));
-}
